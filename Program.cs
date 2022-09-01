bb2432712c8aaf62e024ffd7896a96ad47476cb7
Frode Evensen <frode.evensen@enghouse.com>
2022-09-01 11:57:08 +0200

    Simple project to test memory

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UpgradeAssistantPrefer32Bit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new byte[100000];
+            var memory = new MemoryStream();
+
+            try
+            {
+                while (true)
+                {
+                    memory.Write(array, 0, array.Length);
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"PrivateMemory: {Process.GetCurrentProcess().PrivateMemorySize64:N0}");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
